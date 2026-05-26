33b7167dfab3c945b532e26a2dd74c0c1e6c5172
codewpaule <pauleruvwu@gmail.com>
2026-05-26 17:46:18 +0100

    updated enpoint

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 window.AKILI_CONFIG = {
-  API_BASE: 'https://akili.fly.dev',
+  API_BASE: 'https://api.akili.com.ng',
   // Public client ID is loaded from the server to avoid committing it in source
   GOOGLE_CLIENT_ID: null,
 };
